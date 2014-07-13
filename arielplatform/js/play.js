b00813b2c743bd5db07437d3898240e1851585a3
Sarah-Jeanne Desrochers <sarahjeanne.desrochers7@gmail.com>
2014-07-12 21:18:02 -0400

    tuned up gravity and velocity

--- arielplatform/js/play.js
+++ arielplatform/js/play.js
@@ -24,7 +24,7 @@ var playState = {
 
         this.player.body.damping = 1;
     	this.player.body.collideWorldBounds = true;
-        this.player.body.gravity.y = 1000;
+        this.player.body.gravity.y = 1250;
         this.player.animations.add('left', [0, 1, 2], 10, true);
         this.player.animations.add('right', [4, 5, 6], 10, true);
 
@@ -53,10 +53,10 @@ var playState = {
 
         if(this.cursors.left.isDown){
             this.player.animations.play('left');
-            this.player.body.velocity.x = -125; 
+            this.player.body.velocity.x = -150; 
         } else if(this.cursors.right.isDown){
             this.player.animations.play('right');
-            this.player.body.velocity.x = 125;
+            this.player.body.velocity.x = 150;
         } else {
             this.player.animations.stop();
             this.player.frame = 3;
@@ -67,7 +67,7 @@ var playState = {
         }
         */
         if(this.cursors.up.isDown && this.player.body.onFloor()){       // Key is pressed and player is on floor
-            this.player.body.velocity.y = -350;
+            this.player.body.velocity.y = -375;
             jumpTimer = 1;                                              // Sets it != 0 so player is allowed to jump
         } else if(this.cursors.up.isDown && (jumpTimer != 0)) {
             console.log(jumpTimer);
@@ -78,7 +78,7 @@ var playState = {
                 jumpTimer = 15;
             } else {
                 jumpTimer++;                                            // While player is jumping, some kind of timer ++
-                this.player.body.velocity.y = -350;
+                this.player.body.velocity.y = -375;
             }
         } else if(jumpTimer != 0){                                      // Resets the "timer" when player is on floor && !jumping
             jumpTimer = 0;
@@ -94,5 +94,7 @@ var playState = {
         collectable.kill();
     },
 };
+
+
 
 // Palette from http://www.colourlovers.com/palette/1473/Ocean_Five
